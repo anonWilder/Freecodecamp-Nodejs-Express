bd15b340b3e7647bb6b2252c5b9411b728f17578
anonWilder <austineokafor.e@gmail.com>
2023-10-02 12:07:56 +0100

    responding with a file path

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ let app = express();
 let result = "Hello World"
 console.log(result)
 
-let HANDLER = function (req, res) {
-    res.send('Hello Express')
+// let HANDLER = function (req, res) {
+//     res.send('Hello Express')
+// }
+let IndexView = function (req, res) {
+    res.sendFile("views\index.html")
 }
-app.get("/", HANDLER)
+app.get("/", IndexView)
+console.log(IndexView)
 
 
 
